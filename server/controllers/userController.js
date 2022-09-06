652a212f069490a799f4ef160d665576f05a7b10
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-09-06 17:18:54 +0300

    all endpoint working beside authorization

---
+++ server/controllers/userController.js
@@ -0,0 +1,19 @@
+const { User } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
+class UserController {
+  async registration(req, res) {}
+
+  async login(req, res) {}
+
+  async check(req, res, next) {
+    const { id } = req.query;
+
+    if (!id) {
+      return next(ApiError.badRequest("NO ID"));
+    }
+    return res.send(id);
+  }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
 const sequelize = require("./db");
 const models = require("./models/models");
+const router = require("./routes/router");
+const ErrorMiddleware = require("./middleware/ErrorHandlingMiddleware");
 
 const app = express();
 const PORT = process.env.API_PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-app.use(cors);
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
+app.use("/api", router);
+app.use(ErrorMiddleware);
 
 async function startServer() {
   try {
