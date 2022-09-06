44316f6cd03808ca5a993d9e95a409fcf02f0690
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-09-06 12:53:57 +0300

    first-commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const sequelize = require("./db");
+
+const app = express();
+const PORT = process.env.API_PORT || 5000;
+
+app.use(express.json());
+app.use(cors);
+
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`SERVER STARTED ON PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
